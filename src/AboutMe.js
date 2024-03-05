fbb4ec1beefe33a067002c9e592aa1852aa66f6a
Andrew Yurovchak <andy@yurovchak.net>
2024-03-05 16:15:42 -0500

    added yarn + some minor changes

---
+++ src/AboutMe.js
@@ -0,0 +1,11 @@
+import './AboutMe.css';
+
+function AboutMe() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default AboutMe;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import AboutMe from './AboutMe.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AboutMe />
   </React.StrictMode>
 );
