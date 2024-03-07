9bebed1ef45b7207c5c93c29bc8933d1ca0584fb
Andrew Yurovchak <andy@yurovchak.net>
2024-03-06 21:39:53 -0500

    more about styling + navbar moves

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { AboutHim } from './components/AboutHim/AboutHim.jsx';
@@ -6,14 +6,37 @@ import { Header } from './components/Header.jsx';
 import { Projects } from './components/Projects/Projects.jsx';
 import { Contact } from './components/Contact/Contact.jsx';
 
+function App() {
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  function handleScroll() {
+    console.log(window.scrollY)
+    if(window.scrollY === 0){
+      document.getElementById("header").style.right = "35vw"
+    }else{
+      document.getElementById("header").style.right = "3vw"
+    }
+  }
+
+
+  return <main>
+    <AboutHim />
+    <Projects />
+    <Contact />
+  </main>
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <main>
-      <AboutHim />
-      <Projects />
-      <Contact />
-    </main>
+    <App />
   </React.StrictMode>
 );
