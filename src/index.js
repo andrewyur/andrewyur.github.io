6a23d1e1f0861022fac10171fd0bcc3ed58903ae
Andrew Yurovchak <andy@yurovchak.net>
2024-03-18 18:18:21 -0400

    contact css

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { AboutHim } from './components/AboutHim/AboutHim.jsx';
-import { Header } from './components/Header.jsx';
-import { Projects } from './components/Projects/Projects.jsx';
-import { Contact } from './components/Contact/Contact.jsx';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { AboutHim } from "./components/AboutHim/AboutHim.jsx";
+import { Header } from "./components/Header.jsx";
+import { Projects } from "./components/Projects/Projects.jsx";
+import { Contact } from "./components/Contact/Contact.jsx";
 
 function App() {
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	});
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  });
+	function handleScroll() {
+		console.log(window.scrollY);
+		if (window.scrollY === 0) {
+			document.getElementById("header").style.right = "35vw";
+		} else {
+			document.getElementById("header").style.right = "3vw";
+		}
+	}
 
-  function handleScroll() {
-    console.log(window.scrollY)
-    if(window.scrollY === 0){
-      document.getElementById("header").style.right = "35vw"
-    }else{
-      document.getElementById("header").style.right = "3vw"
-    }
-  }
-
-
-  return <main>
-    <AboutHim />
-    <Projects />
-    <Contact />
-  </main>
+	return (
+		<main>
+			<div id="container">
+				<AboutHim />
+				<Projects />
+				<Contact />
+			</div>
+		</main>
+	);
 }
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Header />
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Header />
+		<App />
+	</React.StrictMode>
 );
