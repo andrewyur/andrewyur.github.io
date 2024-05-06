9fe55259255569b057b1adb42c959e15a7f4fb0e
Andrew Yurovchak <andy@yurovchak.net>
2024-05-05 21:57:46 -0400

    renaming + added react-router dependency

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React, { useRef, useEffect, useLayoutEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { AboutHim } from "./components/AboutHim/AboutHim.jsx";
-import { Header } from "./components/Header.jsx";
-import { Projects } from "./components/Projects/Projects.jsx";
-import { Contact } from "./components/Contact/Contact.jsx";
+import { AboutHim } from "./MainPage/AboutHim/AboutHim.jsx";
+import { Header } from "./MainPage/Header.jsx";
+import { Projects } from "./MainPage/Projects/Projects.jsx";
+import { Contact } from "./MainPage/Contact/Contact.jsx";
 
 function App() {
 	const appRef = useRef();
