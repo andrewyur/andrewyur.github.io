999bf9d960b84c43ad535edd88d45b51c81dd9a5
Andrew Yurovchak <andy@yurovchak.net>
2025-07-24 18:53:09 -0400

    Revert "remove position relative"
    This reverts commit 8e75b37a7bad499b36b660012de1911e58c3229d.

--- src/lib/Toy.svelte
+++ src/lib/Toy.svelte
@@ -127,8 +127,12 @@
         left: 0;
         width: 100%;
         height: 100lvh;
-        z-index: -1;
+        z-index: 0;
         -webkit-user-select: none;
         user-select: none;
     }
+
+    /* :global(canvas) {
+        pointer-events: none;
+    } */
 </style>
